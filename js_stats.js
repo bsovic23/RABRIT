e06c726444a90cdedf5c1db9eed22f9a80e3fb4e
bsovic23 <109878591+bsovic23@users.noreply.github.com>
2022-08-29 13:47:32 -0400

    Add files via upload

---
+++ js_stats.js
@@ -0,0 +1,15 @@
+//function below works with alerting back
+
+//just need to figure out how to reutrn multiple variables
+
+function submit_var(){
+    var sex = document.getElementById("sex").value;
+    if (sex !=""){
+        console.log(sex);
+    }
+};
+//Above will convert the variables to a the correct varname
+
+
+// Using converted variables above determine the stats
+
