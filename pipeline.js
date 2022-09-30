bb409f822b4474ff47d5c65703d3289bc5dc2236
Brit Sovic <bsovic@tulane.edu>
2022-09-30 12:03:21 -0400

    updated pipeline with study choice function

--- pipeline.js
+++ pipeline.js
@@ -6,24 +6,41 @@ var submitPxBtnEl = document.getElementById('submitPx');
 
 var hveListEl = document.getElementById('hveSch');
 
+var historyHVE = [];
 
 // Function
-
 function addPx() {
     var firstNameEl = document.getElementById('fn').value;
     var lastNameEl = document.getElementById('ln').value;
 
+    var studyChosen = "";
+
+    if (document.getElementById('study1').checked == true) {
+        studyChosen = 1;
+    } else if (document.getElementById('study2').checked == true) {
+        studyChosen = 2;
+    } else if (document.getElementById('study3').checked == true) {
+        studyChosen = 3;
+    } else (console.log('a study was not chosen'));
+
     divEl = document.createElement('div');
     h1El = document.createElement('h1');
     h2El = document.createElement('h2');
+    h3El = document.createElement('h3');
 
     h1El.textContent = firstNameEl;
     h2El.textContent = lastNameEl;
+    h3El.textContent = studyChosen;
 
     divEl.appendChild(h1El);
     divEl.appendChild(h2El);
+    divEl.appendChild(h3El);
 
     hveListEl.appendChild(divEl);
+
+    historyHVE.push([firstNameEl, lastNameEl, studyChosen])
+
+    console.log(historyHVE);
 };
 
 submitPxBtnEl.addEventListener("click", addPx);
