e9af2a4c5aef12dbf39fb9a8685111d1aa1c7a26
Brit Sovic <bsovic@tulane.edu>
2023-01-31 13:08:14 -0500

    adding heroku

--- system/excel.js
+++
@@ -1,8 +0,0 @@
-const XLSX = require("xlsx");
-
-const workbook = XLSX.readFile("dummyexcel.xlsx");
-
-let worksheets = {};
-for (const sheetName of workbook.sheetNames) {
-    worksheets[sheetName] = XLSX.utils.sheet_to_html(workbook.Sheets[sheetname]);
-}
